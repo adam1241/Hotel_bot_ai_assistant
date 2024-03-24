2fce720172617b035e78fc9bce94d915ea61a81a
adam1241 <lagsssaibiadam.laz@gmail.com>
2024-03-24 14:16:46 +0100

    intents added and function of collecting clients info in csv added

--- actions/actions.py
+++ actions/actions.py
@@ -34,6 +34,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+import csv
 
 
 class ActionCheckWeather(Action):
@@ -55,3 +56,48 @@ class ActionCheckWeather(Action):
         response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
         dispatcher.utter_message(response)
         return [SlotSet('location', loc)]
+    
+
+def read_csv_file(name,email):
+    filename='Clients.csv'
+    data = []
+    with open(filename, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            Name = row['name']
+            Email = row['email']
+            if name == Name and Email == email :
+                spa = row['spa'] == 'True'
+                luggage = row['luggage'] == 'True'
+                cib = row['cib'] == 'True'
+                bill = float(row['bill'])
+                loyalty = row['loyalty'] == 'True'
+                payment = row['payment'] == 'True'
+                payment_method = row['payment_method']
+                room_booked = row['room_booked']
+                time_room = row['time_room']
+                extra_bed = row['extra_bed'] == 'True'
+                
+                data.append({
+                    'name': Name,
+                    'email': Email,
+                    'spa': spa,
+                    'luggage': luggage,
+                    'cib': cib,
+                    'bill': bill,
+                    'loyalty': loyalty,
+                    'payment': payment,
+                    'payment_method': payment_method,
+                    'room_booked': room_booked,
+                    'time_room': time_room,
+                    'extra_bed': extra_bed
+                })
+                return data
+            
+    return None
+def provide_name(name,email):
+    data= read_csv_file(name,email)
+    if data == None:
+        return 'you are a new client ,welcome to our hotel ! if you are already a client please ask me to repeat collecting your info'
+    else:
+        return 'welcome back sir, how can i help you?'

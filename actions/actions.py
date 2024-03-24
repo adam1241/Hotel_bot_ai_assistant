42510e474e374b42f04375a7fd2f65a67c63f223
Mehdialaoui02 <mehdi2002alaoui@gmail.com>
2024-03-24 16:24:52 +0100

    Finish action booking

--- actions/actions.py
+++ actions/actions.py
@@ -38,8 +38,6 @@ from datetime import datetime
 
 import requests
 import csv
-import pandas
- 
 
 def read_csv_file(name,email):
     filename='Clients.csv'
@@ -90,7 +88,7 @@ class ActionResponseToDate(Action):
     def name(self)-> Text:
         return "action_response_to_date"
     
-    def extract_dates(text) :
+    def extract_dates(self,text) :
         # Regular expression to match dates in the format YYYY/MM/DD
         date_regex = r"(\d{4}([/.-])\d{1,2}([/.-])\d{1,2})"
         matches = re.findall(date_regex, text)
@@ -115,20 +113,34 @@ class ActionResponseToDate(Action):
             dates.sort()
         return dates
     
-    def extract_rooms(in_date,out_date) :
-        pass
+    def extract_rooms(self,in_date,out_date) :
+        available_rooms = []
+        with open('../rooms.csv', 'r') as file:
+            csv_reader = csv.reader(file)
+            for line in csv_reader:
+                dates = line[1].split(',')
+                available = True
+                for date_str in dates:
+                    date = datetime.strptime(date_str, "%Y-%m-%d")
+                    print(date)
+                    if in_date <= date <= out_date:
+                        available = False
+                        break  # Once a date within range is found, move to the next room
+                if available :
+                    available_rooms.append({"id" : line[0], "Type" : line[2], "View type" : line[5], "Price" : line[4], "Capacity" : line[3]})
+        return available_rooms
             
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         user_response = tracker.latest_message.get("text")
         dates = self.extract_dates(user_response)
-        rooms = None
         message = ""
         if len(dates) == 0 :
             message = "Please provide two correct dates in the format YYYY-MM-DD or YYYY/MM/DD"
         elif len(dates) == 1 :
             message = "You provided only one date, please provide two correct dates in the format YYYY-MM-DD or YYYY/MM/DD"
         elif len(dates) == 2 :
-            message = "Here are the available rooms from {dates[0]} to {dates[1]}: {rooms}"
+            rooms = self.extract_rooms(dates[0],dates[1])
+            message = f"Here are the available rooms from {dates[0]} to {dates[1]}: {rooms}"
         else : 
             message = "Too many dates provided"
 
